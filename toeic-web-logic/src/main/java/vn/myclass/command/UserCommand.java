2761c8b21bb57e9ec569240771d3e79451cca499
duykypaul <=>
2019-07-11 17:33:17 +0700

    create files utilbean

---
+++ toeic-web-logic/src/main/java/vn/myclass/command/UserCommand.java
@@ -0,0 +1,8 @@
+package vn.myclass.command;
+
+import vn.myclass.core.dto.UserDTO;
+import vn.myclass.core.web.command.AbstractCommand;
+
+public class UserCommand extends AbstractCommand<UserDTO> {
+
+}
